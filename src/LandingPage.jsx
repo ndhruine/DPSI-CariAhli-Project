c58f1c0e1532c91b8e2b95bac900734914067d66
ASBi03 <2300016092@webmail.uad.ac.id>
2025-07-10 08:47:37 +0700

    perbaikan

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -149,6 +149,7 @@ export default function LandingPage() {
           background: var(--white);
           color: var(--purple);
           border: 2px solid var(--purple);
+          border-radius: 0px 50px 50px 0px;
         }
 
         @media (max-width: 1279px) {
@@ -206,7 +207,7 @@ export default function LandingPage() {
         />
 
         <nav className="nav-menu">
-          <button onClick={() => navigate("/home")} className="active">Beranda</button>
+          <button onClick={() => navigate("/")} className="active">Beranda</button>
           <button onClick={() => navigate("/tentang")}>Tentang Kami</button>
         </nav>
 
